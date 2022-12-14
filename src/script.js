479436600046c5a2a5974a764b6682cba6c133fa
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2022-12-14 13:18:12 +0100

    Added the script.js file

---
+++ src/script.js
@@ -0,0 +1,70 @@
+function formatDate() {
+  let now = new Date();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  let currentDay = days[now.getDay()];
+  let currentHour = now.getHours();
+  if (currentHour < 10) {
+    currentHour = `0${currentHour}`;
+  }
+  let currentMinute = now.getMinutes();
+  if (currentMinute < 10) {
+    currentMinute = `0${currentMinute}`;
+  }
+
+  let formattedDate = `${currentDay} ${currentHour}:${currentMinute}, `;
+  let fullDate = document.querySelector("#date");
+  fullDate.innerHTML = formattedDate;
+}
+
+formatDate();
+
+function currentWeather(response) {
+  console.log(response.data);
+  document.querySelector("#cityName").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+}
+
+function searchEngine(event) {
+  event.preventDefault();
+
+  let city = document.querySelector("#search-input").value;
+  let unit = "metric";
+  let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(currentWeather);
+}
+
+let searchButton = document.querySelector("#search-form");
+searchButton.addEventListener("submit", searchEngine);
