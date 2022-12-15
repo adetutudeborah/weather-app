dec36baf4592e2e6a9d95de9d1df0f6ec065d8e0
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2022-12-15 04:22:16 +0100

    Implemented API integration and temperature conversion

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+
 function formatDate() {
   let now = new Date();
 
@@ -11,21 +12,6 @@ function formatDate() {
     "Saturday"
   ];
 
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-
   let currentDay = days[now.getDay()];
   let currentHour = now.getHours();
   if (currentHour < 10) {
@@ -36,35 +22,77 @@ function formatDate() {
     currentMinute = `0${currentMinute}`;
   }
 
-  let formattedDate = `${currentDay} ${currentHour}:${currentMinute}, `;
-  let fullDate = document.querySelector("#date");
-  fullDate.innerHTML = formattedDate;
+  return `${currentDay} ${currentHour}:${currentMinute}, `;
 }
-
 formatDate();
 
 function currentWeather(response) {
   console.log(response.data);
-  document.querySelector("#cityName").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#cityName");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+function searchCity(city) {
+  let unit = "metric";
+  let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
+  axios.get(apiUrl).then(currentWeather);
 }
 
 function searchEngine(event) {
   event.preventDefault();
-
-  let city = document.querySelector("#search-input").value;
-  let unit = "metric";
-  let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
-
-  axios.get(apiUrl).then(currentWeather);
+  let cityInput = document.querySelector("#search-input");
+  searchCity(cityInput.value);
 }
+
+function convertToFahrenheit() {
+  event.preventDefault();
+
+  let temperature = document.querySelector("#temperature");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperature.innerHTML = Math.round(fahrenheitTemperature);
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+}
+
+function convertToCelsius() {
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(celsiusTemperature);
+
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+}
+
+let celsiusTemperature = null;
 
 let searchButton = document.querySelector("#search-form");
 searchButton.addEventListener("submit", searchEngine);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+searchCity("Lagos");
